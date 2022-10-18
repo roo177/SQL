090d44b0d46d87c885f7aab0f710dbaae1e8303d
roo177 <mahirer@gmail.com>
2022-10-18 18:46:58 +0300

    Update

--- 00. Depo/01. PG - Alter all column names to lowercase.sql
+++ 00. Depo/01. PG - Alter all column names to lowercase.sql
@@ -1,4 +1,4 @@
 SELECT 
-CONCAT('ALTER TABLE ' ,table_schema,'.', table_name, ' RENAME COLUMN ','"',column_name,'"',' TO ',LOWER(column_name)), *
+CONCAT('ALTER TABLE ' ,table_schema,'.', table_name, ' RENAME COLUMN ','"',column_name,'"',' TO ',LOWER(column_name),';'), *
 FROM information_schema.columns 
 WHERE table_schema = 'public';
