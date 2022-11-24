4dc4fb53dac8cd14fe77ef90db483a77d14924c7
roo177 <mahirer@gmail.com>
2022-11-24 16:17:37 +0300

    update

--- 05. Current Budget/25. CB Total Income/01. Tables/03. t_cb_inc_esc_rates_l6.sql
+++ 05. Current Budget/25. CB Total Income/01. Tables/03. t_cb_inc_esc_rates_l6.sql
@@ -13,7 +13,8 @@ CREATE TABLE IF NOT EXISTS public.t_cb_inc_esc_rates_l6
     l_5 character varying(2) COLLATE pg_catalog."default" NOT NULL,
     l_6 character varying(3) COLLATE pg_catalog."default" NOT NULL,
     month date NOT NULL,
-    inc_rate numeric(18,9),
+    inc_rate numeric(18,9) DEFAULT 0,
+    key_r_pc_l6 character varying(50) COLLATE pg_catalog."default" GENERATED ALWAYS AS ((((((((((((((((rep_month)::text || '.'::text) || (pc)::text) || '.'::text) || (l_1)::text) || '.'::text) || (l_2)::text) || '.'::text) || (l_3)::text) || '.'::text) || (l_4)::text) || '.'::text) || (l_5)::text) || '.'::text) || (l_6)::text)) STORED,
     CONSTRAINT t_cb_inc_esc_rates PRIMARY KEY (rep_month, pc, l_1, l_2, l_3, l_4, l_5, l_6, month),
     CONSTRAINT l6 FOREIGN KEY (l_5, l_4, l_3, l_2, l_6, l_1)
         REFERENCES public.c6_code (c_l5, c_l4, c_l3, c_l2, c_l6, c_l1) MATCH SIMPLE
