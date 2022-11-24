525a86140670dad741af338aa6574d7a09cf742e
roo177 <mahirer@gmail.com>
2022-11-24 13:51:14 +0300

    update

--- 05. Current Budget/23. CB Unit Price Expense/01. Tables/02. t_cb_res_up.sql
+++ 05. Current Budget/23. CB Unit Price Expense/01. Tables/02. t_cb_res_up.sql
@@ -13,6 +13,9 @@ CREATE TABLE IF NOT EXISTS public.t_cb_res_up
     up_cost numeric(18,4),
     key_r4_simple character varying(15) COLLATE pg_catalog."default" GENERATED ALWAYS AS ((((((((rs_l1)::text || '.'::text) || (rs_l2)::text) || '.'::text) || (rs_l3)::text) || '.'::text) || (rs_l4)::text)) STORED,
     key_r4 character varying(25) COLLATE pg_catalog."default" GENERATED ALWAYS AS ((((((((((((rep_month)::text || '.'::text) || (pc)::text) || '.'::text) || (rs_l1)::text) || '.'::text) || (rs_l2)::text) || '.'::text) || (rs_l3)::text) || '.'::text) || (rs_l4)::text)) STORED,
+    rcode character varying(12) COLLATE pg_catalog."default",
+    pkey character varying(30) COLLATE pg_catalog."default" GENERATED ALWAYS AS ((((((((((((rep_month)::text || '-'::text) || (pc)::text) || '-'::text) || (rs_l1)::text) || '.'::text) || (rs_l2)::text) || '.'::text) || (rs_l3)::text) || '.'::text) || (rs_l4)::text)) STORED,
+    price_month date,
     CONSTRAINT t_pl_res_up_pkey PRIMARY KEY (rep_month, pc, rs_l1, rs_l2, rs_l3, rs_l4),
     CONSTRAINT r__t_pl_re__r4_code FOREIGN KEY (rs_l1, rs_l2, rs_l4, rs_l3)
         REFERENCES public.r4_code (r_1_code, r_2_code, r_4_code, r_3_code) MATCH SIMPLE
@@ -24,6 +27,10 @@ CREATE TABLE IF NOT EXISTS public.t_cb_res_up
         ON DELETE RESTRICT,
     CONSTRAINT r__t_pl_re__t_rep_m FOREIGN KEY (rep_month)
         REFERENCES public.t_rep_month (rep_month) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+    CONSTRAINT rcode_fkey FOREIGN KEY (rcode)
+        REFERENCES public.r4_code (code_r4) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE RESTRICT
 )
