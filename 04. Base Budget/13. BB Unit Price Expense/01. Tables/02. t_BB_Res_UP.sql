8b32c700fa848aeefa095213711793942e9c0012
roo177 <mahirer@gmail.com>
2022-11-24 17:09:00 +0300

    update

--- 04. Base Budget/13. BB Unit Price Expense/01. Tables/02. t_BB_Res_UP.sql
+++ 04. Base Budget/13. BB Unit Price Expense/01. Tables/02. t_BB_Res_UP.sql
@@ -13,6 +13,7 @@ CREATE TABLE IF NOT EXISTS public.t_bb_res_up
     rep_month character varying(4) COLLATE pg_catalog."default" NOT NULL,
     key_r4_simple character varying(15) COLLATE pg_catalog."default" GENERATED ALWAYS AS ((((((((rs_l1)::text || '.'::text) || (rs_l2)::text) || '.'::text) || (rs_l3)::text) || '.'::text) || (rs_l4)::text)) STORED,
     key_r4 character varying(25) COLLATE pg_catalog."default" GENERATED ALWAYS AS ((((((((((((rep_month)::text || '.'::text) || (pc)::text) || '.'::text) || (rs_l1)::text) || '.'::text) || (rs_l2)::text) || '.'::text) || (rs_l3)::text) || '.'::text) || (rs_l4)::text)) STORED,
+    price_month date,
     CONSTRAINT t_bb_res_up_pkey PRIMARY KEY (pc, rs_l1, rs_l2, rs_l3, rs_l4, rep_month),
     CONSTRAINT r__t_bb_re__r4_code FOREIGN KEY (rs_l1, rs_l2, rs_l4, rs_l3)
         REFERENCES public.r4_code (r_1_code, r_2_code, r_4_code, r_3_code) MATCH SIMPLE
--- 04. Base Budget/13. BB Unit Price Expense/01. Tables/22. t_bb_exp_esc_rates_l6.sql
+++ 04. Base Budget/13. BB Unit Price Expense/01. Tables/22. t_bb_exp_esc_rates_l6.sql
@@ -1,8 +1,8 @@
--- Table: public.t_bb_inc_esc_rates_l6
+-- Table: public.t_bb_exp_esc_rates_l6
 
--- DROP TABLE IF EXISTS public.t_bb_inc_esc_rates_l6;
+-- DROP TABLE IF EXISTS public.t_bb_exp_esc_rates_l6;
 
-CREATE TABLE IF NOT EXISTS public.t_bb_inc_esc_rates_l6
+CREATE TABLE IF NOT EXISTS public.t_bb_exp_esc_rates_l6
 (
     rep_month character varying(4) COLLATE pg_catalog."default" NOT NULL,
     pc character varying(3) COLLATE pg_catalog."default" NOT NULL,
@@ -13,24 +13,27 @@ CREATE TABLE IF NOT EXISTS public.t_bb_inc_esc_rates_l6
     l_5 character varying(2) COLLATE pg_catalog."default" NOT NULL,
     l_6 character varying(3) COLLATE pg_catalog."default" NOT NULL,
     month date NOT NULL,
-    inc_rate numeric(18,9),
+    exp_rate numeric(18,9),
     key_r_pc_l6 character varying(50) COLLATE pg_catalog."default" GENERATED ALWAYS AS ((((((((((((((((rep_month)::text || '.'::text) || (pc)::text) || '.'::text) || (l_1)::text) || '.'::text) || (l_2)::text) || '.'::text) || (l_3)::text) || '.'::text) || (l_4)::text) || '.'::text) || (l_5)::text) || '.'::text) || (l_6)::text)) STORED,
-    CONSTRAINT t_bb_inc_esc_rates PRIMARY KEY (rep_month, pc, l_1, l_2, l_3, l_4, l_5, l_6, month),
-    CONSTRAINT l6 FOREIGN KEY (l_5, l_4, l_2, l_3, l_6, l_1)
-        REFERENCES public.c6_code (c_l5, c_l4, c_l2, c_l3, c_l6, c_l1) MATCH SIMPLE
+    CONSTRAINT t_bb_exp_esc_rates PRIMARY KEY (rep_month, pc, l_1, l_2, l_3, l_4, l_5, l_6, month),
+    CONSTRAINT l6 FOREIGN KEY (l_5, l_4, l_3, l_2, l_6, l_1)
+        REFERENCES public.c6_code (c_l5, c_l4, c_l3, c_l2, c_l6, c_l1) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT,
+        ON DELETE RESTRICT
+        NOT VALID,
     CONSTRAINT pc FOREIGN KEY (pc)
         REFERENCES public.t_001_projects (p_code) MATCH SIMPLE
         ON UPDATE CASCADE
-        ON DELETE RESTRICT,
+        ON DELETE RESTRICT
+        NOT VALID,
     CONSTRAINT repmonth FOREIGN KEY (rep_month)
         REFERENCES public.t_rep_month (rep_month) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE RESTRICT
+        NOT VALID
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.t_bb_inc_esc_rates_l6
+ALTER TABLE IF EXISTS public.t_bb_exp_esc_rates_l6
     OWNER to ictasadmin;
